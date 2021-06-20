62b2f6c8246a1a840634338fb9c153468f2d5753
kasutaja <martin.raba@mail.ee>
2021-06-21 00:09:01 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <?php wp_head(); ?>
+
+  </head>
+  <body>
+  <div class="navigation">
+        <input type="checkbox" class="navigation__checkbox" id="navi-toggle">
+        <label for="navi-toggle" class="navigation__btn">
+            <span class="navigation__icon"></span>
+        </label>
+        <div class="navigation__background">&nbsp;</div>
+        <nav class="navigation__nav">
+            <ul class="navigation__list">
+                <li class="navigation__item"><a class="navigation__link" href="#">Home</a></li>
+                <li class="navigation__item"><a class="navigation__link" href="#">About us</a></li>
+                <li class="navigation__item"><a class="navigation__link" href="#">Curriculums</a></li>
+                <li class="navigation__item"><a class="navigation__link" href="#">Events</a></li>
+            </ul>
+        </nav>
+    </div>
+  <header class="site-header">
+    <div class="main-navigation">
+    <h1 class="header_university_name heading-1">
+          <strong>Fictional</strong> University
+        </h1>
+        <nav class="main-navigation__nav">
+            <ul class="main-navigation__list min-list">
+                <li class="main-navigation__item"><a class="main-navigation__link" href="#">Home</a></li>
+                <li class="main-navigation__item"><a class="main-navigation__link" href="#">About us</a></li>
+                <li class="main-navigation__item"><a class="main-navigation__link" href="#">Curriculums</a></li>
+                <li class="main-navigation__item"><a class="main-navigation__link" href="#">Events</a></li>
+            </ul>
+        </nav>
+    </div>
+  
+    
+      <div class="banner-container">
+        
+
+      </div>
+    </header>
